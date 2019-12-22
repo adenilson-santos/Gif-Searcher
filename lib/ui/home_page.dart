19dc41859a5fbe34ba1219e7c3a841bd3713ce2a
Adenilson Santos <adenilson.santos@gocase.com.br>
2019-12-22 10:16:28 -0300

    showing more gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -14,15 +14,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   String _search;
-  int _offset;
+  final _limitDefault = 20;
+  int _offset = 0, _limit = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
+    _limit = _limitDefault;
 
     if(_search == null) {
-      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=QB7WvjnEE7fNW1UMXDGmLkbftsQlpnMF&limit=20&rating=G');
+      _limit = _limitDefault;
+      response = await http.get('https://api.giphy.com/v1/gifs/trending?api_key=QB7WvjnEE7fNW1UMXDGmLkbftsQlpnMF&$_limit=20&rating=G');
     } else {
-      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=QB7WvjnEE7fNW1UMXDGmLkbftsQlpnMF&q=$_search&limit=20&offset=$_offset&rating=G&lang=en');
+      _limit = _limitDefault - 1;
+      response = await http.get('https://api.giphy.com/v1/gifs/search?api_key=QB7WvjnEE7fNW1UMXDGmLkbftsQlpnMF&q=$_search&limit=$_limit&offset=$_offset&rating=G&lang=en');
     }
 
     return json.decode(response.body);
@@ -61,11 +65,84 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
-              textAlign: TextAlign.center
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                });
+              },
             ),
           ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: (context, snapshot) {
+                switch(snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      width: 200.0,
+                      alignment: Alignment.center,
+                      child: LinearProgressIndicator(),
+                    );
+                  default:
+                   if(snapshot.hasError) return Container();
+                   else return _createGifTable(context, snapshot);
+                }
+              },
+            ),
+          )
         ], 
       )
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+      ),
+      itemCount: _limitDefault,
+      itemBuilder: (context, index) {
+        if(_search == null) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            )
+          );
+        } else {
+          if(index == _limit) {
+            return GestureDetector(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.add, color: Colors.white, size: 80.0,),
+                  Text('Mostrar mais...', style: TextStyle(color: Colors.white),),
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += _limit;
+                });
+              },
+            );
+          } else {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data['data'][index]['images']['fixed_height']['url'],
+                height: 300.0,
+                fit: BoxFit.cover,
+              )
+            );
+          }
+        }
+      },
     );
   }
 }
